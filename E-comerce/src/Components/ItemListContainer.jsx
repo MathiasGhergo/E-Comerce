d0cee4eff6d4a5b052baebc27f47592113cd925e
ghergomathias@hotmail.com <ghergomathias@hotmail.com>
2023-08-30 11:41:36 -0300

    Agregando listado

--- E-comerce/src/Components/ItemListContainer.jsx
+++ E-comerce/src/Components/ItemListContainer.jsx
@@ -1,8 +1,17 @@
-import React from "react"; 
+import { useState, useEffect } from "react"; 
+import{ getproducts } from "../services";
 import ItemList from "./ItemList";
 
 const ItemListContainer =() => {
-    return <ItemList items={[]}/>;
+    const {items, setItems} = useState([]);
+    useEffect(() => {
+        getproducts().then(response => {
+            setItems(response.data);
+        });
+    }, []);
+
+
+    return <ItemList items={[items]}/>;
 };
 
 export default ItemListContainer;
